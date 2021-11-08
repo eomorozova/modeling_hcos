92e7ef9d59be1e89462da234243a30c31f2973ad
Ekaterina Morozova <morozova.e.o@gmail.com>
2021-11-08 13:43:00 -0500

    Sample intrinsic and synaptic conductances from a uniform distribution

--- hco_xfind.m
+++ hco_xfind.m
@@ -1,28 +1,27 @@
 % use xfind to sample random neual models and log them to disk
 
 % Searching for half-center oscillators with
-% Escape mechanism (Vth=-55 mV), one value of synaptic conductance gsyn=5nS
+% Escape mechanism (Vth=-55 mV),
 % and neurons with conducnatces randomly sampled from  a uniform distribution
 
 % set up a neuron/ network
 p = xfind;
 p.x = HalfCenterOscillator;
-
+p.x.t_end = 90e3; % msec
 
 % define parameters to randomly sample and bounds
 
-p.ParameterNames = p.x.find('*gbar');
+p.ParameterNames = [p.x.find('*gbar'); p.x.find('*gmax')];
 
 A=0.005; % area of the neuron (mm^2)
 
-%         CaF  CaS     H     KA      KCa    K1      K2       Kir  Leak   NaP   NaV     
+%         CaF  CaS     H     KA      KCa    K1      K2       Kir  Leak   NaP   NaV  Synapse   
 % neuron #1
-Upper1 = [10,   6.4,   8,   200,   200,   200,    200,     200,   16,   15,   400]./(A*1000);
-% neuron #2
-Upper2 = [10,   6.4,   8,   200,   200,   200,    200,     200,   16,   15,   400]./(A*1000);
+Upper = [10,   6.4,   8,   200,   200,   200,    200,     200,   16,   15,   400]./(A*1000);
 
-p.Upper=[Upper1, Upper2];
-p.Lower = zeros(22,1);
+
+p.Upper=Upper;
+p.Lower = zeros(11,1);
 
 
 % defining a smaple function: uniform random sampler
@@ -36,7 +35,14 @@ p.SampleFcn = @p.uniformRandom;
 p.SimFcn = @p.measureFiringRate;
 
 
-% we only want to keep non silent networks
+% keep all the networks
 p.DiscardFcn = @(data) data <= 0;
 
-p.simulate
+%p.simulate
+
+p.parallelSearch
+
+% wait for a bit
+pause(1200)
+
+cancel(p.workers)
